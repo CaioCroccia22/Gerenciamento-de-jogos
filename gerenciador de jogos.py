668227da46f6234190003625c89b22546632f600
caiocroccia22 <caiocroccia22@gmail.com>
2023-10-16 20:40:06 -0300

    Criação do arquivo

---
+++ gerenciador de jogos.py
@@ -0,0 +1,12 @@
+#Exercicio final do módulo
+#Gerenciamento de jogadores e times
+# -> Escreva um código em python que realize o gerenciamento de jogadores
+#Siga os requisitos:
+#1 - A opção de listar os times deve mostrar o indice , o nome e a quantindade de jogadores
+#2 - A opção de adicionar um time deve pedir um nome para o time que será cadastrado
+#3 - A opção de remover um time deve pedir o indice especifico do time que foi cadastrado
+#4 - A opção de adicionar um jogador em um time deve pedir um indice do time que foi cadastrado
+#5 - A opção de remover um jogador em um time deve pedir um indice do time que foi cadastrado e utilizar
+#esse indice para remover o jogador cadastrado no time
+#6 - A opção de listar os jogadores de um time deve ser informado o indice de um time
+# e listar os jogadores que foram associados a ele.
