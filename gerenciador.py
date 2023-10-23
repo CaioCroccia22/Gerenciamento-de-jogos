6d0af8f0f78b346dbe8e434df0d9d9d4ad35b234
caiocroccia22 <caiocroccia22@gmail.com>
2023-10-22 22:19:32 -0300

    inclusão e exclusão de times

--- gerenciador.py
+++ gerenciador.py
@@ -11,22 +11,30 @@
 #6 - A opção de listar os jogadores de um time deve ser informado o indice de um time
 # e listar os jogadores que foram associados a ele.
 
-Teams = {}
-Team = {}
+teams = {}
+team = {}
 
 done = False
 
 def plusTeam():
     name = input("Digite o nome do time:\n")
     players = input("Digite o números de jogadores\n")
-    Team["name"] = name
-    Team["players"] = players
-    print(Team)
-    Teams["Team"] = Team
+    team["name"] = name
+    team["players"] = players
+    teams["Team"] = team
 
-
+#Função que lista os times
 def listTeam():
+    #enumerate - ter acesso ao indice de um determinado valor do dicionario
+    for i, team in enumerate(teams.values()):
+        print(f"{i+1}, {team['name']} ({len(team['players'])} jogadores)")
     
+
+def listPlayers(team):
+    print(f"Jogadores do {team['name']}")
+    for i, players in enumerate(team['players']):
+        print(f"{i+1}.{players}")
+
 
 
 while done == False:
@@ -35,9 +43,29 @@ while done == False:
     print("2 - Voce deseja remover algum time? \n")
     print("3 - Voce deseja listar os time? \n")
     print("4 - Voce deseja adicionar jogador do time? \n")
-    print("6 - Voce deseja remover jogador do time? \n")
-    print("7 - Voce deseja remover jogador do time? \n")
-    number = int(input("Digite qual opção você deseja:\n"))
-    if number == 1:
-        plusTeam()
-        done == True
+    print("5 - Voce deseja remover jogador do time? \n")
+    print("6 - Voce deseja listar os jogadores de um time? \n")
+    print("7 - Sair \n")
+    choices = input(">")
+    if choices == "1":
+        team_name = input("Digite o nome do time:\n")
+        teams[team_name] = {'name': team_name, 'players': [] }
+        print("Time adicionado.")
+    elif choices == "2":
+        listTeam()
+        team_num = int(input("Digite o número do time que deseja remover: \n"))
+        if team_num <= len(teams):
+            team_name = list(teams.keys()[team_num - 1])
+
+    elif choices == "3":
+        pass
+    elif choices == "4":
+        pass
+    elif choices == "5":
+        pass
+    elif choices == "6":
+        pass
+    elif choices == "7":
+        done = True
+    else:
+        print("Opção inválida")
